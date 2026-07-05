bcb4bcba44469a3f1c117d1c5794760b6edad5f4
Rohit Ravindra <dev.rohitravindra1@gmail.com>
2026-07-06 00:06:58 +0530

    feat: patching now runs git apply

--- internal/git/patches.go
+++ internal/git/patches.go
@@ -2,21 +2,36 @@ package git
 
 import "fmt"
 
-func checkPatchValidity(patchFile string) error {
+func checkPatchValidity(filePath string) error {
 	checkApplyArgs := append(
 		[]string{
 			"apply",
 			"--check",
 		},
-		patchFile,
+		filePath,
 	)
 
 	return executeGitCommand(checkApplyArgs)
 }
 
+func patchFile(filePath string) error {
+	patchFileArgs := append(
+		[]string{
+			"apply",
+		},
+		filePath,
+	)
+
+	return executeGitCommand(patchFileArgs)
+}
+
 func ApplyPatches(filesToPatch []string) error {
 	for _, filePath := range filesToPatch {
 		if err := checkPatchValidity(filePath); err != nil {
+			fmt.Printf("\t[Error] while trying to patch file %s : %s\n", filePath, err.Error())
+			continue
+		}
+		if err := patchFile(filePath); err != nil {
 			fmt.Printf("\t[Error] while trying to patch file %s : %s\n", filePath, err.Error())
 		}
 	}
